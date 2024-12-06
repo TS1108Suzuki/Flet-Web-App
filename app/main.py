add5321c1ec74204be8f744cd63b06306f3de59b
TS1109Suzuki <s.takumi.suzuki@gmail.com>
2024-12-06 20:41:22 +0900

    初回コミット

---
+++ app/main.py
@@ -0,0 +1,33 @@
+# Flet app example
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+
+# Web モードで起動
+if __name__ == "__main__":
+    ft.app(target=main, view=ft.WEB_BROWSER)
